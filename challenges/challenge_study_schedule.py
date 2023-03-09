65096a53eb9a6884799c37d795db552b8cb4a0a1
Martin Bachmann <martinpezbach@gmail.com>
2023-03-09 15:57:37 -0300

    refactor study schedule challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    if not permanence_period:
-        return None
-
-    if not target_time:
+    if not permanence_period or not target_time:
         return None
 
     counter = 0
     for period in permanence_period:
-        if type(period[0]) != int:
-            return None
-        if type(period[1]) != int:
+        if type(period[0]) != int or type(period[1]) != int:
             return None
         if period[0] <= target_time <= period[1]:
             counter += 1
