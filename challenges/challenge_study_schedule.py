20598dba1f18f62299d5ab89a351f79809dc4c60
Martin Bachmann <martinpezbach@gmail.com>
2023-03-09 15:37:30 -0300

    study schedule challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not permanence_period:
+        return None
+
+    if not target_time:
+        return None
+
+    counter = 0
+    for period in permanence_period:
+        if type(period[0]) != int:
+            return None
+        if type(period[1]) != int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            counter += 1
+
+    return counter
