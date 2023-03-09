97e4f8a77855062ea8ea839af669e3bcd05dcbf0
Martin Bachmann <martinpezbach@gmail.com>
2023-03-09 15:55:54 -0300

    encrypt message test challenge

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+
+VALID_MESSAGE = "String"
 
 
 def test_encrypt_message():
-    pass
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(VALID_MESSAGE, "1")
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(12, 1)
+
+    assert encrypt_message(VALID_MESSAGE, 3) == "rtS_gni"
+    assert encrypt_message(VALID_MESSAGE, 2) == "gnir_tS"
+    assert encrypt_message(VALID_MESSAGE, 20) == "gnirtS"
